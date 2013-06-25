86d146d8a0c309af55c0a67aac914c97df275b8a
hugoxian <hugo.xian@qq.com>
2013-06-25 09:49:24 +0800

    修复多处调用ImageFetcher addTask重复加载图片的bug

--- src/com/hugo/image/fetcher/plugin/ImageFetcher.java
+++ src/com/hugo/image/fetcher/plugin/ImageFetcher.java
@@ -52,9 +52,9 @@ public class ImageFetcher {
 		int cpuCount = Runtime.getRuntime().availableProcessors();
 		// 也可以根据网络状况，譬如wifi，gprs等决定初始化线程池数
 		// 根据CPU的核数初始化线程池
-		// this.executorService = Executors.newFixedThreadPool(cpuCount + 1);
+		this.executorService = Executors.newFixedThreadPool(cpuCount + 1);
 		// 单线程池，主要是为了观察效果，测试用
-		this.executorService = Executors.newSingleThreadExecutor();
+		// this.executorService = Executors.newSingleThreadExecutor();
 		this.fetchers = new ArrayList<Fetchable>();
 		// 加载三个图片加载器，注意顺序，优先从内存缓存中取，其次SD卡缓存中存，最后从网络中获取
 		this.fetchers.add(new MemoryCahceFetcher(cacheSize));
@@ -150,8 +150,22 @@ public class ImageFetcher {
 			return;
 		}
 
+		// 如果从内存中直接取到图片
+		Bitmap bitmap = null;
+		try {
+			bitmap = this.fetchers.get(0).fetch(photoUrl);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (bitmap != null) {
+			// 还在UI线程中，可直接设置图片
+			imageView.setImageBitmap(bitmap);
+			return;
+		}
+
 		imageView.setTag(photoUrl);
-		tasks.put(Integer.toString(imageView.hashCode()), imageView);
+		// 由于Android中listView中的item是复用的，这个tasks里面存的都是能看到的ImageView
+		this.tasks.put(Integer.toString(imageView.hashCode()), imageView);
 
 		// 如果不是锁住的话，立即加载图片
 		if (!this.isLock) {
@@ -163,12 +177,18 @@ public class ImageFetcher {
 	 * 执行加载图片任务
 	 */
 	public synchronized void doTasks() {
-		for (Map.Entry<String, ImageView> entry : tasks.entrySet()) {
-			ImageInfoHolder holder = new ImageInfoHolder();
-			holder.imageView = entry.getValue();
-			holder.photoUrl = (String) entry.getValue().getTag();
-			this.executorService.execute(new FetchRunnable(holder));
+		for (Map.Entry<String, ImageView> entry : this.tasks.entrySet()) {
+			ImageView iv = entry.getValue();
+			// 以防ImageView给垃圾回收器回收
+			if (iv != null) {
+				ImageInfoHolder holder = new ImageInfoHolder();
+				holder.imageView = entry.getValue();
+				holder.photoUrl = (String) entry.getValue().getTag();
+				this.executorService.execute(new FetchRunnable(holder));
+			}
 		}
+		// 请空task列表
+		this.tasks.clear();
 	}
 
 	/**
